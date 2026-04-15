6bf8e524e5ed7c2e32e067b0c074aa4cb9e6a419
Federico Shelna <fshelna@c1r3p7.42firenze.it>
2026-04-15 11:33:40 +0200

    mod

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import sys
+import arcade
 
 from parser import Parser
 from dijkstra import Dijkstra
 from drone import Drone
-from simulation import Simulation 
+from simulation import Simulation
 from graph import Graph
+from renderer import Renderer
 
 
 def main() -> None:
     # maps/challenger/01_the_impossible_dream.txt
-    parser: Parser = Parser("maps/easy/02_simple_fork.txt")
+    parser: Parser = Parser("linear_path.txt")
     graph: Graph = parser.parse()
+    renderer: Renderer = Renderer()
     if not graph:
         raise SystemExit("Graph not found")
     dijkstra: Dijkstra = Dijkstra()
     simulation: Simulation = Simulation(graph=graph)
 
     drones: list[Drone] = [Drone(n) for n in range(graph.nb_drones)]
-    
+
     for drone in drones:
         schedule = dijkstra.path(graph, simulation.table)
         drone.path = [item for item, turn in schedule if not isinstance(item, str)]
@@ -25,6 +28,9 @@ def main() -> None:
 
     for el in simulation.table:
         print(el)
+
+    renderer.setup()
+    arcade.run()
 
 if __name__ == '__main__':
     main()
--- renderer.py
+++ renderer.py
@@ -11,48 +11,29 @@ COLOR_GOAL = arcade.color.GREEN
 COLOR_DEADEND = arcade.color.RED
 COLOR_NORMAL = arcade.color.BLUE
 
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+WINDOW_TITLE = "Fly In"
 
 class Renderer(arcade.Window):
     def __init__(self):
-        super().__init__(WINDOW_W, WINDOW_H, WINDOW_T)
-        self.background_color = BACKGROUND_COLOR
-        self.top_text: list[arcade.Text] = [
-            arcade.Text('Fly In', 1, WINDOW_H - 20, arcade.color.WHITE, 22),
-            arcade.Text('Fly In', 96, WINDOW_H - 20, arcade.color.WHITE, 22),
-            arcade.Text('Fly In', 96 * 2, WINDOW_H - 20, arcade.color.WHITE, 22),
-            arcade.Text('Fly In', 96 * 3, WINDOW_H - 20, arcade.color.WHITE_SMOKE, 22),
-            arcade.Text('Fly In', 96 * 4, WINDOW_H - 20, arcade.color.WHITE_SMOKE, 22),
-            arcade.Text('Fly In', 96 * 5, WINDOW_H - 20, arcade.color.WHITE_SMOKE, 22)
-        ]
-        self.animation_speed: int = 100
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        self.grid: list[list[int]] = []
+        for y in range(5):
+            self.grid[y].append([])
+            for x in range(5):
+                self.grid[y][x].append(x - y)
 
 
     def setup(self) -> None:
+        arcade.set_background_color(arcade.color.BLACK)
         ...
-
     def on_draw(self) -> None:
         self.clear()
-        # Animation
-        for txt in self.top_text:
-            txt.draw()
-        arcade.draw_line(0, WINDOW_H - 32, WINDOW_W, WINDOW_H - 32, arcade.color.WHITE, 2)
-
-    def on_key_press(self, symbol, modifiers):
-        if symbol == arcade.key.ESCAPE:
-            arcade.close_window()
-
-        if symbol == arcade.key.SPACE:
-            self.animation_speed += 20
-            if self.animation_speed > 200:
-                self.animation_speed = 100
-
-
-    def on_update(self, delta_time):
-        for txt in self.top_text:
-            txt.x += delta_time * self.animation_speed
-            if txt.x > WINDOW_W:
-                txt.x = -120
-        return super().on_update(delta_time)
-
-    def run(self, view = None):
-        return super().run(view)
+        arcade.draw_circle_filled(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 24, arcade.color.RED)
+        for y in len(self.grid):
+            for x in len(self.grid[0]):
+                if self.grid[y][x] < 0:
+                    arcade.draw_rect_filled(arcade.Rect(0,0,0,0, 16,16, x * 16, y * 16), arcade.color.RED)
+                else:
+                    arcade.draw_rect_filled(arcade.Rect(0,0,0,0, 16,16, x * 16, y * 16), arcade.color.GREEN)
